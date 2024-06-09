fd9ad6168d54ae3cda76f1933cff259c78f814f5
uqtony <uqtony@gmail.com>
2024-06-10 05:55:05 +0800

    [yocto][8mmevk]Add 8mm-evk build and flash scripts
    MACHINE=imx8mmevk
    DISTRO=fsl-imx-xwayland
    build_folder=build_fsl-imx-xwayland
    image_type=imx-image-full

---
+++ uqtony_arm_scripts/eideal/yocto/build-8mmevk.sh
@@ -0,0 +1,2 @@
+MACHINE=imx8mmevk DISTRO=fsl-imx-xwayland source imx-setup-release.sh -b build_fsl-imx-xwayland
+bitbake imx-image-full
---
+++ uqtony_arm_scripts/eideal/yocto/flash-8mmevk.sh
@@ -0,0 +1,2 @@
+cp ~/bin/uuu .
+sudo ./uuu -b emmc_all imx-boot-imx8mmevk-sd.bin-flash_evk imx-image-full-imx8mmevk.rootfs.wic.zst/*
